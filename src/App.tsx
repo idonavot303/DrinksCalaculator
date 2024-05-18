ff7942a9c518212e792e76daefda87051b8aa6a4
idonavot303 <idonavot303@gmail.com>
2024-05-18 18:02:23 +0300

    added type-script

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import s from './App.module.scss';
 
 function App() {
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './styles/_global.scss';
 import './styles/resets.scss';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <App />
